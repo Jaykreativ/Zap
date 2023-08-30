7323a968b16e58acd1b393eea95e183321cb9286
Jan Zaugg <zauggjunk4@outlook.com>
2023-08-30 10:42:31 +0200

    Add Vulkan Dependency

--- Setup.bat
+++ Setup.bat
@@ -2,6 +2,11 @@ mkdir Dependencies
 cd Dependencies
 git clone https://github.com/Jaykreativ/Vulkan-Framework --recursive
 git clone https://github.com/glfw/glfw --recursive
+curl https://sdk.lunarg.com/sdk/download/1.3.261.0/windows/VulkanSDK-1.3.261.0-Installer.exe -O VulkanSDK-1.3.261.0-Installer.exe
+start /W VulkanSDK-1.3.261.0-Installer.exe -ArgumentList '/s'
+del VulkanSDK-1.3.261.0-Installer.exe
+echo Vulkan Dir: %VULKAN_SDK%
 cd..
-#cmake .
-#cmake --build .
+cmake .
+cmake --build .
+pause
