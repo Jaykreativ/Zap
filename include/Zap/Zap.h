03001186b2a050ea01ea56c49f632c034705c448
Jaykreativ <janzaugg@bluewin.ch>
2023-11-05 18:26:37 +0100

    save

--- include/Zap/Zap.h
+++ include/Zap/Zap.h
@@ -1,10 +1,40 @@
 #pragma once
 #define GLM_FORCE_LEFT_HANDED
 #include "VulkanFramework.h"
+#include "PxPhysicsAPI.h"
 
 //TODO add standart renderer for windows with no renderer
 namespace Zap {
     class Window;
+
+    class Base {
+    public:
+        void init();
+
+        void terminate();
+
+        static Base* createBase(const char* applicationName);
+
+        static void releaseBase();
+
+        static Base* getBase();
+
+    private:
+        Base(const char* applicationName);
+        ~Base();
+
+        bool m_isInit;
+
+        const char* m_applicationName;
+
+        //physx variables
+        physx::PxFoundation* m_pxFoundation;
+        physx::PxPhysics* m_pxPhysics;
+        physx::PxScene* m_pxScene;
+
+        static Base m_engineBase;
+        static bool m_exists;
+    };
 
     namespace objects {
         static std::vector<Window*> windows;
@@ -14,23 +44,4 @@ namespace Zap {
         VkFormat getColorFormat();
         VkFormat getDepthStencilFormat();
     }
-
-    class Base {
-    public:
-        Base(const char* applicationName);
-
-        ~Base();
-
-        void init();
-
-        void terminate();
-
-    private:
-        bool m_isInit;
-
-        const char* m_applicationName;
-
-        static bool m_exists;
-    };
-
 }
