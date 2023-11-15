7631685ece0f7787a79fb953f9cca824fffa45be
Jaykreativ <janzaugg@bluewin.ch>
2023-11-15 19:03:31 +0100

    add imgui

--- Setup.bat
+++ Setup.bat
@@ -2,6 +2,7 @@ mkdir Dependencies
 cd Dependencies
 git clone -b save https://github.com/Jaykreativ/VulkanFramework --recursive
 git clone https://github.com/glfw/glfw --recursive
+git clone https://github.com/ocornut/imgui --recursive
 git clone https://github.com/g-truc/glm --recursive
 git clone https://github.com/assimp/assimp --recursive
 git clone https://github.com/NVIDIA-Omniverse/PhysX --recursive
