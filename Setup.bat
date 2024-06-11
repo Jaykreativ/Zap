55a0b3d3cf2872abbb4064fe3826d07f684d16b6
Jaykreativ <janzaugg@bluewin.ch>
2024-06-11 18:29:42 +0200

    Physx cmake subdir

--- Setup.bat
+++ Setup.bat
@@ -7,8 +7,8 @@ git clone https://github.com/g-truc/glm --recursive
 git clone https://github.com/assimp/assimp --recursive
 git clone https://github.com/NVIDIA-Omniverse/PhysX --recursive
 cd PhysX/physx
-start /W generate_projects.bat
-cmake --build compiler/vc17win64
+::start /W generate_projects.bat
+::cmake --build compiler/vc17win64
 cd../..
 ::curl https://sdk.lunarg.com/sdk/download/1.3.261.0/windows/VulkanSDK-1.3.261.0-Installer.exe -O VulkanSDK-1.3.261.0-Installer.exe
 ::start /W VulkanSDK-1.3.261.0-Installer.exe -ArgumentList '/s'
